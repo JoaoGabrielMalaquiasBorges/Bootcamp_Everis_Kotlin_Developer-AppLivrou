c02e3330b19c699fa0336b15bd2a936cda2aed69
JoaoGabrielMalaquiasBorges <joaomalaquiassb@gmail.com>
2021-04-05 01:29:34 -0300

    update SQLiteDB class

--- app/src/main/java/com/example/applivrou/SQLiteDB.kt
+++ app/src/main/java/com/example/applivrou/SQLiteDB.kt
@@ -7,13 +7,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import java.net.URL
 
-class SQLiteDB(context: Context) : SQLiteOpenHelper(context, "NAME", null, 1), BookshelfDAO {
-    companion object {
-        private const val NAME = "SQLiteDB"
-        private const val VERSION = 1
-    }
-
-    private var booksList = arrayListOf<Book>()
+class SQLiteDB(context: Context) : SQLiteOpenHelper(context, "SQLiteDB", null, 1), BookshelfDAO {
+    private val booksList: ArrayList<Book> = ArrayList()
 
     private val TABLE_NAME = "BOOKS"
 
@@ -22,14 +17,13 @@ class SQLiteDB(context: Context) : SQLiteOpenHelper(context, "NAME", null, 1), B
             ID INTEGER NOT NULL,
             TITLE VARCHAR NOT NULL,
             AUTHOR VARCHAR NOT NULL,
-            EDITION VARCHAR NOT NULL,
             RELEASE_DATE VARCHAR NOT NULL,
             PUBLISHER VARCHAR NOT NULL,
             DESCRIPTION TEXT NOT NULL,
             COVER VARCHAR NOT NULL,
             CATEGORY VARCHAR NOT NULL,
             
-            PRIMARY KEY ${'('}ID AUTOINCREMENT${')'}
+            PRIMARY KEY ${'('}ID${')'}
         )
     """
 
@@ -42,9 +36,11 @@ class SQLiteDB(context: Context) : SQLiteOpenHelper(context, "NAME", null, 1), B
     }
 
     override fun getBooks(category: String): ArrayList<Book> {
-        val db = readableDatabase ?: return arrayListOf()
+        booksList?.clear() ?: booksList
+        
+        val db = readableDatabase ?: return booksList
         val sql = "SELECT * FROM $TABLE_NAME"
-        var cursor = db.rawQuery(sql, null) ?: return arrayListOf()
+        var cursor = db.rawQuery(sql, null) ?: return booksList
 
         while (cursor.moveToNext()) {
             val url =
@@ -59,7 +55,41 @@ class SQLiteDB(context: Context) : SQLiteOpenHelper(context, "NAME", null, 1), B
                 )
             )
         }
+        
+        db.close()
 
         return booksList
+    }
+    
+    fun saveBooks(booksList: ArrayList<JsonObject>, category: String) {
+        val db = writableDatabase ?: return
+        var sql: String
+        
+        booksList.forEach { book ->
+            sql = """
+                INSERT INTO $TABLE_NAME (
+                    ID,
+                    TITLE,
+                    AUTHOR,
+                    RELEASE_DATE,
+                    PUBLISHER,
+                    DESCRIPTION,
+                    COVER,
+                    CATEGORY
+                ) VALUES (
+                    ${book.get("id").asString},
+                    ${book.get("title").asString},
+                    ${book.get("author").asString},
+                    ${book.get("releaseDate").asString},
+                    ${book.get("publisher").asString},
+                    ${book.get("description").asString},
+                    ${book.get("cover").asString},
+                    $category
+                )
+            """
+        
+            db.rawQuery(sql, null) ?: return
+            db.close()
+        }
     }
 }
